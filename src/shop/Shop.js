c0769df534f26c7b21a983e5fe4e59a8ec16b038
Dima <budya2012@gmail.com>
2021-05-26 17:02:24 +0300

    draft: stage page 129

---
+++ src/shop/Shop.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {CategoryNavigation} from './CategoryNavigation';
+import {ProductList} from './ProducList';
+
+export class Shop extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col bg-dark text-white">
+            <div className="navbar-brand">
+             SPORTS STORE
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-3 p-2">
+            <CategoryNavigation !!!!!!!!!!!!!!!!!
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
